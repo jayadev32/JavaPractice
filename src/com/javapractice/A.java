59a0498d321901f9dba88b90083b1c94fe53e878
jayadev32 <jayadevan32@gmail.com>
2022-09-12 22:51:58 +0530

    B works

--- src/com/javapractice/A.java
+++ src/com/javapractice/A.java
@@ -5,6 +5,20 @@ public class A {
 		System.out.println("2%");
 
 	}
-	
 
+	private void fixed() {
+		System.out.println("2%");
+	}
+
+	private void fixed1() {
+		System.out.println("2%");
+	}
+
+	private void fixed2() {
+		System.out.println("2%");
+	}
+
+	private void fixed3() {
+		System.out.println("2%");
+	}
 }
